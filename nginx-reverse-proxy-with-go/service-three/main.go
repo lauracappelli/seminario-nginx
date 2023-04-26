b481741074aca119bd28c96b9e1c9377aa5e767d
lcappelli <laura.cappelli@cnaf.infn.it>
2023-04-26 12:33:08 +0200

    Add exercises and config examples

---
+++ nginx-reverse-proxy-with-go/service-three/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Laura, welcome to the Service 3!")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func main() {
+    handleRequests()
+}
